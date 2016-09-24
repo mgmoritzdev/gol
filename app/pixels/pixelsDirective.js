cfcc39bfe1b392da8693dbd27ddd1e065280ae87
Marcos Moritz <marcosmoritz@gmail.com>
2016-09-24 18:27:31 -0300

    Fix di error

--- app/pixels/pixelsDirective.js
+++ app/pixels/pixelsDirective.js
@@ -7,7 +7,7 @@
     return {
       restrict: 'E',
       templateUrl: 'app/pixels/pixels.html',
-      controller: PixelsController,
+      controller: 'PixelsController',
       controllerAs: 'vm',
       scope: {
         rows: '&',
