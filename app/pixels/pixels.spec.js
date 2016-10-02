05e6f2efc84ba58eff5465b695813167dfaa65a3
Marcos Moritz <marcosmoritz@gmail.com>
2016-10-02 12:04:47 -0300

    Fix tests for pixels

--- app/pixels/pixels.spec.js
+++ app/pixels/pixels.spec.js
@@ -1,6 +1,34 @@
 describe('PixelsController', function() {
 
-  beforeEach(module('app'));
+  var $interval;
+  var $timeout;
+  var golService;
+
+  beforeEach(function() {
+    module('app');
+
+    module(function($provide) {
+      $provide.factory('golService', function() {
+        return {
+          getNeighboursCount: function() {},
+          getNewPixelColor: getWhite,
+          run: function() {},
+          getScore: getScore
+        };
+
+        function getScore() {
+          return {
+            'red': 10,
+            'blue': 20
+          };
+        }
+
+        function getWhite() {
+          return 'white';
+        }
+      });
+    });
+  });
 
   var $controller;
 
@@ -8,15 +36,20 @@ describe('PixelsController', function() {
     $controller = _$controller_;
   }));
 
+  var controller;
+
+  beforeEach(inject(function($interval, $timeout, _golService_) {
+    golService = _golService_;
+    controller = $controller('PixelsController', {
+      '$interval': $interval,
+      '$timeout': $timeout,
+      'golService': golService
+    });
+  }));
+
   describe('vm.definition', function() {
 
-    var controller;
-
-    beforeEach(function() {
-      controller = $controller('PixelsController', {});
-    });
-
-    it('number of rows should be equal to difinitino[0]', function() {
+    it('number of rows should be equal to definition[0]', function() {
       expect(controller.rows.length).toBe(controller.definition[0]);
       expect(controller.rows[0].pixels.length).toBe(controller.definition[1]);
     });
@@ -33,9 +66,26 @@ describe('PixelsController', function() {
     it('toggled black pixel should be white', function() {
       var testPixel = controller.rows[0].pixels[0];
       testPixel.color = 'black';
-      controller.togglePixel(testPixel);
+      controller.pixelClicked(testPixel, 'white');
 
       expect('white').toBe(testPixel.color);
     });
   });
+
+  // describe('vm.rows', function() {
+
+  //   var controller;
+
+  //   beforeEach(function() {
+  //     controller = $controller('PixelsController', {});
+  //   });
+
+  //   it('toggled black pixel should be white', function() {
+  //     var testPixel = controller.rows[0].pixels[0];
+  //     testPixel.color = 'black';
+  //     controller.togglePixel(testPixel);
+
+  //     expect('white').toBe(testPixel.color);
+  //   });
+  // });
 });
